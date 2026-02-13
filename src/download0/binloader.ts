8021295ab3e6c9f0b3da83639f8614c1d9e4915e
mrdude2478 <10487145+mrdude2478@users.noreply.github.com>
2026-02-13 08:10:37 +0000

    chore: eslint auto-fix

--- src/download0/binloader.ts
+++ src/download0/binloader.ts
@@ -629,7 +629,6 @@ export function binloader_init () {
 
     return true
   }
-  
 
   // Network binloader (fallback)
   function bl_network_loader () {
@@ -761,17 +760,15 @@ export function binloader_init () {
   }
 
   if (!binloader_auto_run_done) {
-  	binloader_auto_run_done = true
-  	if (!is_jailbroken) {
-  		bin_loader_main()
-    }
-    else {
-    	if (bl_file_exists('/data/payloads/elfldr.elf')) {
-    		bl_load_from_file('/data/payloads/elfldr.elf')
-    	}
-    	else {
-    		log(payload + ' not found!')
-    	}
+    binloader_auto_run_done = true
+    if (!is_jailbroken) {
+      bin_loader_main()
+    } else {
+      if (bl_file_exists('/data/payloads/elfldr.elf')) {
+        bl_load_from_file('/data/payloads/elfldr.elf')
+      } else {
+        log(payload + ' not found!')
+      }
     }
   }
 
